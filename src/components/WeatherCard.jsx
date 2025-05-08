41a9b7960e7d9e6d0ce3c975ed738d9b69fdc70c
danny <jobs4danyul@gmail.com>
2025-05-08 13:26:19 -0400

    Tidied up weatherCard

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,11 +1,30 @@
-// src/components/WeatherCard.jsx
+import styles from './WeatherCard.module.css';
+
 export default function WeatherCard({ data }) {
-    return (
-      <div className="weather-card">
-        <h2>{data.name}, {data.sys.country}</h2>
-        <p>🌡️ {data.main.temp}°C</p>
-        <p>💨 {data.wind.speed} m/s</p>
-        <p>☁️ {data.weather[0].description}</p>
-      </div>
-    );
-  }
+  return (
+    <div className={styles.weatherCard}>
+      <h2>{data.name}, {data.sys.country}</h2>
+      <p>
+        <span className={styles.descriptor}>🌡️ Temperature: </span> {Math.round(data.main.temp)}°C
+      </p>
+      <p>
+        <span className = {styles.descriptor}> ☂️ Real Feel:</span> {data.main.feels_like}°C
+      </p>
+      <p>
+        <span className = {styles.descriptor}> ☀️ High:</span> {data.main.temp_max}°C
+      </p>
+      <p>
+        <span className = {styles.descriptor}> ❄️ Low:  </span> {data.main.temp_min}°C
+      </p>
+      <p>
+        <span className = {styles.descriptor}> 💧 Humidity:  </span> {data.main.humidity}%
+      </p>
+      <p>
+        <span className = {styles.descriptor}>💨 Wind speed: </span> {data.wind.speed}m/s
+      </p>
+      <p>
+        <span className = {styles.descriptor}>☁️ Weather Condition: </span> {data.weather[0].description}
+      </p>
+    </div>
+  );
+}
